8faa0707ec78f95645927e1928a0253181084b0d
Christian Rivas <christian_rv2001@hotmail.com>
2024-02-06 18:27:54 -0500

    Functional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import Question from "./question"
+import { promises as fs } from 'fs';
 
-export default function Home() {
+const fetchData = async () =>{
+  const lang = "es"
+  var temp1 = await fs.readFile(process.cwd() + '/src/lib/questions.json', 'utf8');
+  const questions = JSON.parse(temp1);
+  var temp1 = await fs.readFile(process.cwd() + '/src/lib/options.json', 'utf8');
+  const options = JSON.parse(temp1);
+  
+  return [questions[lang],options[lang]]
+}
+
+export default async function Home() {
+  const [questions,options] = await fetchData()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-primary">
-      <div className="w-100" style={{background:"gray"}}>
-        <Question id={1}></Question>
-
+    <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-dark">
+      <div className="center-elems w-100">
+        <Question questions={questions} options={options}></Question>
       </div>
     </main>
   );
--- src/app/question.tsx
+++ src/app/question.tsx
@@ -2,42 +2,66 @@
 import React from "react";
 import {Button, Container, Row, Col} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface IOption{
+    op:string[],
+    index:number
+}
 interface IProps{
-    id:number
+    questions:string[],
+    options:IOption[]
 }
 
-const check = (id:string, comp:boolean) =>{
-    const elem = document.getElementById(id)
-    if (comp){
-        elem!.style.background="green"
-    }
-    else{
-        elem!.style.background="red"
-    }
-}
-const Answer = (i:string, text:string) =>{
-    return (
-    <Col>
-        <Button id={i} onClick={() => check(i,i==correct)} className='g-col-3 m-3 p-8' >{text}</Button>
-    </Col>
-    )
-}
-const question = '¿?'
-const options = ["a. Opción 1","b. Opción 2","c. Opción 3","d. Opción 4"]
-const correct = "1"
+
 export default function Question(props:IProps){
+
+    const check = (id:string, comp:boolean) =>{
+        const elem = document.getElementById(id)
+        if (comp){
+            elem!.style.background="green"
+            setStateNext(false)
+        }
+        else{
+            elem!.style.background="red"
+        }
+    }
+    const nextQuestion = () =>{
+        for (let i=0;i<4;i++){
+            document.getElementById(i.toString())!.style.background="grey"
+        }
+        setStateNext(true); setIndex(index+1); setOptions(props.options[index])
+    }
+
+    const Answer = (i:number, text:string) =>{
+        let iString = i.toString()
+        return (
+        <Col>
+            <Button id={iString} onClick={() => check(iString.toString(),i==options.index)} className='g-col-3 m-3 p-8 w-75 h-75' >{text}</Button>
+        </Col>
+        )
+    }
+    const [index, setIndex] = React.useState(0)
+    const [stateNext, setStateNext] = React.useState(true)
+    const [options, setOptions] = React.useState(props.options[0])
+
     return(
-        <Container className="rounded w-75 p-2 bg-success text-center">
-        <h2 className="bg-danger w-auto text-center m-3 rounded">{"Pregunta "+props.id+": "+question}</h2> 
+        <Container className="rounded w-75 p-2 bg-primary text-center">
+        <h2 className="w-auto text-center m-3 rounded">{"Pregunta "+(index+1)+": "+props.questions[index]}</h2> 
         <Row className="mx-0">
-        {Answer("0",options[0])}
-        {Answer("1",options[1])}
+        {Answer(0,options.op[0])}
+        {Answer(1,options.op[1])}
         </Row>
         
         <Row className="mx-0">
-        {Answer("2",options[2])}
-        {Answer("3",options[3])}
+        {Answer(2,options.op[2])}
+        {Answer(3,options.op[3])}
         </Row>
+        <Row>
+            <div>
+                <Button className="btn-light float-right m-3" onClick={nextQuestion} disabled={stateNext}>Siguiente</Button>
+            </div>
+        </Row>
+
       </Container>
     )
 }
